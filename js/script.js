9476c6d2a1b9b847c403558e80e415adaa9fb73b
emukungu <enmukungu@gmail.com>
2018-07-01 15:28:45 +0300

    store the ooutput in an array for display in the web page.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 //BUSINESS LOGIC
 function pingPong(number){
   //takes a number from a user and returns a range of numbers from 1 to the chosen number with the following exceptions:
-  //Numbers divisible by 3 are replaced with "ping"; Numbers divisible by 5 are replaced with "pong";Numbers divisible by 15 are replaced with "pingpong"
-  newArray = []
+  //Numbers divisible by 3 are replaced with "ping"; Numbers divisible by 5 are replaced with "pong";
+  //Numbers divisible by 15 are replaced with "pingpong"
+  var answer
+  var newArray = []
   for(var i = 1; i<=number; i++){
+
     newArray.push(i)
   }
   return newArray
@@ -24,7 +27,7 @@ $(document).ready(
 
         $("#numbers-list").append("<li>"+result+"</li>")
         $(".display").show()
-
+        $("#ping").val("")
 
       }
     )
