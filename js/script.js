bae9f665dc837fff62ba4767b9c5bfb2118ab954
emukungu <enmukungu@gmail.com>
2018-07-01 03:32:36 +0300

    add the function for the first BDD.

---
+++ js/script.js
@@ -0,0 +1,17 @@
+//BUSINESS LOGIC
+function pingPong(number){
+  //takes a number from a user and returns a range of numbers from 1 to the chosen number with the following exceptions:
+  //Numbers divisible by 3 are replaced with "ping"; Numbers divisible by 5 are replaced with "pong";Numbers divisible by 15 are replaced with "pingpong"
+
+  for(var i = 1; i<=number; i++){
+    i
+  }
+  return i
+
+}
+
+
+
+
+
+//USER LOGIC
